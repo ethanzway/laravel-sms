9fff32cafa78bffb24d19da8a332f2ccdc86464b
ethanzway <gw19900524@gmail.com>
2018-02-17 21:34:52 +0800

    migration

---
+++ src/Drivers/Mandao.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Gw19900524\Sms\Drivers;
+
+use Exception;
+use GuzzleHttp\Client as HttpClient;
+use gw19900524\sms\Support\Log;
+
+class Mandao
+{
+    protected $parameters = [
+        'url' => '',
+        'port' => '',
+        'sn' => '',
+        'secret' => '',
+    ];
+    
+    /**
+     *
+     * Send the message to the mobile.
+     * 
+     * @param  string   $mobile
+     * @param  string  $content
+     * @return bool
+     */
+    public function send($mobile, $content)
+    {
+        $argv = array( 
+            'sn' => $this->getSn(),
+            'pwd' => strtoupper(md5($this->getSn().$this->getSecret())), 
+            'mobile' => $mobile ?? '',
+            'content' => $content ?? '',
+            'ext' => '',
+            'stime' => '',
+            'rrid' => '',
+        );
+        try {
+            $result = (new HttpClient())->request('POST', $this->getUrl(),  ['form_params' => $argv])->getBody()->getContents();
+            preg_match('/<string xmlns="http:\/\/tempuri.org\/">(.*)<\/string>/', $result, $matches);
+            if ($matches[1] > 1) {
+                Log::info('Success', array('mobile' => substr($mobile, 0, 3) . '****' . substr($mobile, -4), 'content' => iconv('gb2312', 'utf-8//IGNORE', $content)));
+            } else {
+                Log::warning('Failed', array('mobile' => substr($mobile, 0, 3) . '****' . substr($mobile, -4), 'content' => iconv('gb2312', 'utf-8//IGNORE', $content)));
+            }
+        } catch (Exception $e) {
+            Log::error('Error', array('mobile' => substr($mobile, 0, 3) . '****' . substr($mobile, -4), 'content' => iconv('gb2312', 'utf-8//IGNORE', $content)));
+        }
+    }
+
+    /**
+     * @param  string $key
+     * @return mixed
+     */
+    public function getParameter($key)
+    {
+        return $this->parameters[$key];
+    }
+
+    /**
+     * @param  string $key
+     * @param  mixed  $value
+     * @return $this
+     */
+    public function setParameter($key, $value)
+    {
+        
+        $this->parameters[$key] = $value;
+
+        return $this;
+    }
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setUrl($value)
+    {
+        return $this->setParameter('url', $value);
+    }
+    
+    /**
+     *
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->getParameter('url');
+    }
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setPort($value)
+    {
+        return $this->setParameter('port', $value);
+    }
+    
+    /**
+     *
+     * @return mixed
+     */
+    public function getPort()
+    {
+        return $this->getParameter('port');
+    }
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setSn($value)
+    {
+        return $this->setParameter('sn', $value);
+    }
+    
+    /**
+     *
+     * @return mixed
+     */
+    public function getSn()
+    {
+        return $this->getParameter('sn');
+    }
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setSecret($value)
+    {
+        return $this->setParameter('secret', $value);
+    }
+    
+    /**
+     *
+     * @return mixed
+     */
+    public function getSecret()
+    {
+        return $this->getParameter('secret');
+    }
+}
---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Gw19900524\Sms;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider
+{
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defere = true;
+    
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish config
+        $configPath = __DIR__ . '/Config/config.php';
+        $this->publishes([$configPath => config_path('laravel-sms.php')], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('sms', function($app){
+            $factory = new \Gw19900524\Sms\SmsFactory;
+            $manager = new \Gw19900524\Sms\SmsManager($app, $factory);
+            return $manager;
+        });
+        $this->app->alias('sms', SmsManager::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['sms'];
+    }
+}
